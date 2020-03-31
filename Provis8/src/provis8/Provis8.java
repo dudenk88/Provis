4052877fe7adf640905e5a5e1e600fdc47419b6f
dudenk88 <faridnais88@gmail.com>
2020-03-31 22:48:27 +0700

    Tugas_Provis

---
+++ Provis8/src/provis8/Provis8.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package provis8;
+
+/**
+ *
+ * @author Dudenk
+ */
+public class Provis8 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       frameutama futama= new frameutama();
+       futama.tampil("10118332 - Nais Farid");
+       jframe jf = new jframe();
+       
+      jf.setVisible(true);
+    }
+    
+}
